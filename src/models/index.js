b99ca65c355638d23e2082afd3d945285f3a614d
Jongwon Park <jwparktom@gmail.com>
2021-07-15 02:39:02 -0400

    Delete unused files/folders

--- src/models/index.js
+++
@@ -1,5 +0,0 @@
-// module.exports.Token = require('./token.model');
-// module.exports.User = require('./user.model');
-module.exports = {
-  // Course: require('./course.model'),
-};
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const docsRoute = require('./docs.route');
 const coursesRoute = require('./course.route');
 const config = require('../../config/config');
 
@@ -9,26 +8,11 @@ const defaultRoutes = [
   {
     path: '/course',
     route: coursesRoute,
-  }
-];
-
-const devRoutes = [
-  // routes available only in development mode
-  {
-    path: '/docs',
-    route: docsRoute,
   },
 ];
 
 defaultRoutes.forEach((route) => {
   router.use(route.path, route.route);
 });
-
-/* istanbul ignore next */
-if (config.env === 'development') {
-  devRoutes.forEach((route) => {
-    router.use(route.path, route.route);
-  });
-}
 
 module.exports = router;
